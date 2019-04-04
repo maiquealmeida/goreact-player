a9c3cfb5baf15b3265bbb691acd54fa66e387876
Maique Almeida <maique.malmeida@gmail.com>
2019-04-04 17:57:37 -0400

    Commit inicial: Estilizacoes dos componentes

--- src/App.js
+++ src/App.js
@@ -1,26 +1,22 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component, Fragment } from 'react';
+import GlobalStyle from './styles/global';
+
+import Sidebar from './components/Sidebar';
+import Player from './components/Player';
+import { Wrapper, Container } from './styles/components';
 
 class App extends Component {
+  componentDidMount() {}
+
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
-      </div>
+      <Wrapper>
+        <GlobalStyle />
+        <Container>
+          <Sidebar />
+        </Container>
+        <Player />
+      </Wrapper>
     );
   }
 }
---
+++ src/components/Player/index.js
@@ -0,0 +1,71 @@
+import React from 'react';
+import Slider from 'rc-slider';
+import {
+  Container, Current, Volume, Progress, Controls, Time, ProgressSlider,
+} from './styles';
+
+import VolumeIcon from '../../assets/images/volume.svg';
+import ShuffleIcon from '../../assets/images/shuffle.svg';
+import BackwardIcon from '../../assets/images/backward.svg';
+import PlayIcon from '../../assets/images/play.svg';
+import PauseIcon from '../../assets/images/pause.svg';
+import ForwardIcon from '../../assets/images/forward.svg';
+import RepeatIcon from '../../assets/images/repeat.svg';
+
+const Player = props => (
+  <Container>
+    <Current>
+      <img
+        src="https://images-na.ssl-images-amazon.com/images/I/81GtprxyiRL._SX355_.jpg"
+        alt="Cover"
+      />
+      <div>
+        <span>No More Hell to Pay</span>
+        <small>Stryper</small>
+      </div>
+    </Current>
+    <Progress>
+      <Controls>
+        <button type="button">
+          <img src={ShuffleIcon} alt="Shuffle" />
+        </button>
+        <button type="button">
+          <img src={BackwardIcon} alt="Backward" />
+        </button>
+        <button type="button">
+          <img src={PlayIcon} alt="Play" />
+        </button>
+        <button type="button">
+          <img src={ForwardIcon} alt="Forward" />
+        </button>
+        <button type="button">
+          <img src={RepeatIcon} alt="Repeat" />
+        </button>
+      </Controls>
+
+      <Time>
+        <span>1:39</span>
+        <ProgressSlider>
+          <Slider
+            railStyle={{ backgrund: '#404040', borderRadius: 10 }}
+            trackStyle={{ background: '#1ED760' }}
+            handleStyle={{ border: 0 }}
+          />
+        </ProgressSlider>
+        <span>4:27</span>
+      </Time>
+    </Progress>
+
+    <Volume>
+      <img src={VolumeIcon} alt="Volume" />
+      <Slider
+        railStyle={{ background: '#404040', borderRadius: 10 }}
+        trackStyle={{ background: '#FFF' }}
+        handleStyle={{ display: 'none' }}
+        // value={100}
+      />
+    </Volume>
+  </Container>
+);
+
+export default Player;
